1d0f3fc1301fea1df7384906a0c2fe5b9c04859f
Cindy Rodríguez-Llivipuma <c.rodriguezllivipuma@gmail.com>
2025-05-26 16:03:51 -0400

    added delete feature for vocab entries - pending edit feature

--- client/components/journalsComponents/Vocabulary.tsx
+++ client/components/journalsComponents/Vocabulary.tsx
@@ -10,9 +10,15 @@ interface VocabEntry {
 }
 
 const Vocabulary: React.FC = () => {
-  const [vocabEntries, setVocabEntries] = useState<VocabEntry[] | undefined>([{ word: 'Amour', lexCat: 'Noun', meaning: 'Love' }, { word: 'Amour', lexCat: 'Noun', meaning: 'Love' }, { word: 'Amour', lexCat: 'Noun', meaning: 'Love' },]); 
+  const [vocabEntries, setVocabEntries] = useState<VocabEntry[]>([]); 
   const [openLexCatDropdown, setOpenLexCatDropdown] = useState<boolean>(false); 
   const [selectedLexCat, setSelectedLexCat] = useState<string | undefined>(undefined); 
+  const [wordEntered, setWordEntered] = useState<string>(''); 
+  const [meaningEntered, setMeaningEntered] = useState<string>(''); 
+  const [noLexCatSelected, setNoLexCatSelected] = useState<boolean>(false); 
+  const [noWordEntered, setNoWordEntered] = useState<boolean>(false); 
+  const [noMeaningEntered, setNoMeaningEntered] = useState<boolean>(false); 
+  const [newVocabEntry, setNewVocabEntry] = useState<VocabEntry | undefined>(undefined); 
 
   // [{ word: 'Amour', lexCat: 'Noun', meaning: 'Love' }, { word: 'Amour', lexCat: 'Noun', meaning: 'Love' }, { word: 'Amour', lexCat: 'Noun', meaning: 'Love' },]
 
@@ -27,6 +33,53 @@ const Vocabulary: React.FC = () => {
     setSelectedLexCat(category);
     setOpenLexCatDropdown(false);
   }; 
+
+  // function to handle new vocab entry 
+  const handleNewVocabEntry = (): void => {
+    //! create components for warnings
+    // check if all fields required for a vocab entry are completed. If they are not, show user warning
+    // no word entered
+    if (!wordEntered) return setNoWordEntered(true); 
+    // no lex cat selected
+    if (!selectedLexCat) return setNoLexCatSelected(true); 
+    // no meaning entered
+    if (!meaningEntered) return setNoMeaningEntered(true); 
+      
+    // if all fields are valid, initialize a variable to store all fields 
+    const newVocabEntry: VocabEntry = {
+      word: `${wordEntered}`, 
+      lexCat: `${selectedLexCat}`, 
+      meaning: `${meaningEntered}`, 
+    };
+
+    //! when database is built - send new vocab entry  
+
+
+    // copy current vocab entries state and reset state to include new vocab entry 
+    setVocabEntries([...vocabEntries, newVocabEntry]); 
+
+    // reset all fields to an empty string
+    setWordEntered(''); 
+    setSelectedLexCat(undefined); 
+    setMeaningEntered(''); 
+  }
+
+  // function to edit a vocab entry 
+  const handleVocabEntryEdit = (entryIndex: number): void => {
+
+  
+  }
+
+  // function to delete a vocab entry 
+  const handleVocabEntryDelete = (entryIndex:number): void => {
+    // initialize variable to store updated vocab entries array 
+    const updatedVocabEntries: VocabEntry[] = [...vocabEntries];
+    const removedVocabEntry: VocabEntry[] = updatedVocabEntries.splice(entryIndex, 1);
+    
+    // set vocab entries state with updated variable 
+    setVocabEntries(updatedVocabEntries); 
+  }
+
 
   return (
     <>
@@ -51,18 +104,18 @@ const Vocabulary: React.FC = () => {
             </thead>
           </table>
           <div className='scroll-body'>
-            {vocabEntries !== undefined ? 
+            {vocabEntries.length > 0 ? 
               <table className='body-table'>
                 <tbody className='table-body'>
                   {vocabEntries.map((entry, index) => (
-                    <tr className='table-row'>
+                    <tr className='table-row' key={index}>
                       <td className='table-data td-1'>{entry.word}</td>
                       <td className='table-data td-2'>{entry.lexCat}</td>
                       <td className='table-data td-3'>{entry.meaning}</td>
                       <td className='td-4'>
                         <div className='icons-div'>
-                          <FontAwesomeIcon className='icon pencil' icon={faPencil} />
-                          <FontAwesomeIcon className='icon trash' icon={faTrash} />
+                          <FontAwesomeIcon className='icon pencil' icon={faPencil} onClick={() => handleVocabEntryEdit(index)}/>
+                          <FontAwesomeIcon className='icon trash' icon={faTrash} onClick={() => handleVocabEntryDelete(index)}/>
                         </div>
                       </td>
                     </tr>
@@ -77,7 +130,7 @@ const Vocabulary: React.FC = () => {
           </div>
           <div className='input-div'>
             {/* <div className='input-container'> */}
-            <input placeholder='Enter word' className='td-input td-i-1'></input>
+            <input placeholder='Enter word' className='td-input td-i-1' type='text' value={wordEntered} onChange={(e) => setWordEntered(e.target.value)}></input>
             <div className='td-div'>
               <h4 className='td-h4'>{selectedLexCat || 'Select'}</h4>
               {/* <div className='td-div-2'> */}
@@ -94,12 +147,12 @@ const Vocabulary: React.FC = () => {
               </>
               }
             </div>
-            <input placeholder='Enter meaning' className='td-input td-i-2'></input>
+            <input placeholder='Enter meaning' className='td-input td-i-2' type='text' value={meaningEntered} onChange={(e) => setMeaningEntered(e.target.value)}></input>
             <div className='empty-div'></div>
           </div>
         </div>
         <div className='btn-div'>
-          <button className='add-entry-btn'>Add Entry</button>
+          <button className='add-entry-btn' onClick={handleNewVocabEntry}>Add Entry</button>
         </div>
       </section>
     </>
